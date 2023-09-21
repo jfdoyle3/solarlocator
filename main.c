30007de3c0bc4200be1f56e3d570558fe3b8d78f
Jim Doyle <36973560+jfdoyle3@users.noreply.github.com>
2023-09-20 21:23:31 -0400

    Learning pointers

--- main.c
+++ main.c
@@ -4,11 +4,18 @@
 
 void main(void)
 {
-
+  /*
   SunInfo();
   printf("\n\nThis is String\n\n");
   SunInfoStr();
   // parse SunInfo() json response
+  */
 
+  int n=300;
+  int *ptr=NULL;
+  ptr=&n;
+
+
+  printf("%d",*ptr);
 }
 
