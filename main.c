e289ae2ff86346bbcfa4fb4ef60c55fe95313bed
Jim Doyle <indellibleink@gmail.com>
2023-10-04 20:59:25 -0400

    uncommented code have new pepos running

--- main.c
+++ main.c
@@ -2,21 +2,20 @@
 // #include "SunAPI.h"
 
 
-int main(void)
+int main()
 {
-  /*
+
   SunInfo();
+  /*
   printf("\n\nThis is String\n\n");
+
   SunInfoStr();
+
   // parse SunInfo() json response
   */
 
-  int n=300;
-  int *ptr=NULL;
-  ptr=&n;
+  printf("Boo!!");
 
-
-  printf("%d",*ptr);
 
   return 0;
 }
